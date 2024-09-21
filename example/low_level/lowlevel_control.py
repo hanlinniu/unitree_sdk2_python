0354cfd9ae9c1ff8e260cecb1f551984c736369d
hanlinniu <niuhanlin@icloud.com>
2024-09-21 17:20:43 +0100

    update

--- example/low_level/lowlevel_control.py
+++ example/low_level/lowlevel_control.py
@@ -43,10 +43,10 @@ if __name__ == '__main__':
         cmd.motor_cmd[go2.LegID["RL_2"]].tau = 1.0 # target toque is set to 1N.m
 
         # Position(rad) control, set RL_0 rad
-        cmd.motor_cmd[go2.LegID["RL_0"]].q = 0.0  # Taregt angular(rad)
-        cmd.motor_cmd[go2.LegID["RL_0"]].kp = 10.0 # Poinstion(rad) control kp gain
-        cmd.motor_cmd[go2.LegID["RL_0"]].dq = 0.0  # Taregt angular velocity(rad/ss)
-        cmd.motor_cmd[go2.LegID["RL_0"]].kd = 1.0  # Poinstion(rad) control kd gain
+        cmd.motor_cmd[go2.LegID["RL_0"]].q = 0.0  # Target angular(rad)
+        cmd.motor_cmd[go2.LegID["RL_0"]].kp = 10.0 # Position(rad) control kp gain
+        cmd.motor_cmd[go2.LegID["RL_0"]].dq = 0.0  # Target angular velocity(rad/ss)
+        cmd.motor_cmd[go2.LegID["RL_0"]].kd = 1.0  # Position(rad) control kd gain
         cmd.motor_cmd[go2.LegID["RL_0"]].tau = 0.0 # Feedforward toque 1N.m
         
         cmd.crc = crc.Crc(cmd)
