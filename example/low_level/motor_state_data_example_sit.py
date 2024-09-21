c8abd9229f2520c7bd71fedeeb47295dbf0a99b4
hanlinniu <niuhanlin@icloud.com>
2024-09-21 21:36:14 +0100

    update

---
+++ example/low_level/motor_state_data_example_sit.py
@@ -0,0 +1,40 @@
+# Front right hip  ID: 0
+[MotorState_(mode=1, q=-0.023987174034118652, dq=0.01937761902809143, ddq=0.0, tau_est=-0.07421484589576721, q_raw=0.0, dq_raw=0.0, ddq_raw=0.0, temperature=46, lost=0, reserve=[0, 602]), 
+ # Front right thigh  ID: 1
+ MotorState_(mode=1, q=1.2516571283340454, dq=-0.03100419044494629, ddq=0.0, tau_est=0.12369140982627869, q_raw=0.0, dq_raw=0.0, ddq_raw=0.0, temperature=31, lost=0, reserve=[0, 602]), 
+# Front right calf  ID: 2
+ MotorState_(mode=1, q=-2.803433895111084, dq=0.014154086820781231, ddq=0.0, tau_est=-0.04741504043340683, q_raw=0.0, dq_raw=0.0, ddq_raw=0.0, temperature=35, lost=0, reserve=[0, 602]), 
+
+
+# Front left hip  ID: 3
+ MotorState_(mode=1, q=0.02418339252471924, dq=0.05813286080956459, ddq=0.0, tau_est=-0.049476563930511475, q_raw=0.0, dq_raw=0.0, ddq_raw=0.0, temperature=44, lost=0, reserve=[0, 602]), 
+ # Front left thigh  ID: 4 
+ MotorState_(mode=1, q=1.2704291343688965, dq=0.011626571416854858, ddq=0.0, tau_est=-0.049476563930511475, q_raw=0.0, dq_raw=0.0, ddq_raw=0.0, temperature=31, lost=0, reserve=[0, 602]), 
+ # Front left calf  ID: 5
+ MotorState_(mode=1, q=-2.8065619468688965, dq=-0.046506285667419434, ddq=0.0, tau_est=0.04741504043340683, q_raw=0.0, dq_raw=0.0, ddq_raw=0.0, temperature=36, lost=0, reserve=[0, 602]), 
+ 
+
+ # Rear right hip  ID: 6
+ MotorState_(mode=1, q=-0.32952290773391724, dq=-0.046506285667419434, ddq=0.0, tau_est=0.07421484589576721, q_raw=0.0, dq_raw=0.0, ddq_raw=0.0, temperature=49, lost=0, reserve=[0, 602]), 
+ # Rear right hip  ID: 7
+ MotorState_(mode=1, q=1.2829283475875854, dq=0.003875523805618286, ddq=0.0, tau_est=0.024738281965255737, q_raw=0.0, dq_raw=0.0, ddq_raw=0.0, temperature=31, lost=0, reserve=[0, 602]), 
+ # Rear right hip  ID: 8
+ MotorState_(mode=1, q=-2.811363935470581, dq=0.014154086820781231, ddq=0.0, tau_est=-0.09483008086681366, q_raw=0.0, dq_raw=0.0, ddq_raw=0.0, temperature=32, lost=0, reserve=[0, 602]), 
+ 
+
+ # Rear left hip  ID: 9
+ MotorState_(mode=1, q=0.3244363069534302, dq=-0.023253142833709717, ddq=0.0, tau_est=0.0, q_raw=0.0, dq_raw=0.0, ddq_raw=0.0, temperature=51, lost=0, reserve=[0, 602]), 
+ # Rear left hip  ID: 10
+ MotorState_(mode=1, q=1.2946155071258545, dq=-0.011626571416854858, ddq=0.0, tau_est=-0.049476563930511475, q_raw=0.0, dq_raw=0.0, ddq_raw=0.0, temperature=31, lost=0, reserve=[0, 602]), 
+ # Rear left hip  ID: 11
+ MotorState_(mode=1, q=-2.808568000793457, dq=-0.02022012509405613, ddq=0.0, tau_est=0.04741504043340683, q_raw=0.0, dq_raw=0.0, ddq_raw=0.0, temperature=33, lost=0, reserve=[0, 602]), 
+
+
+MotorState_(mode=0, q=0.0, dq=0.0, ddq=0.0, tau_est=0.0, q_raw=0.0, dq_raw=0.0, ddq_raw=0.0, temperature=0, lost=0, reserve=[0, 0]), 
+MotorState_(mode=0, q=0.0, dq=0.0, ddq=0.0, tau_est=0.0, q_raw=0.0, dq_raw=0.0, ddq_raw=0.0, temperature=0, lost=0, reserve=[0, 0]), 
+MotorState_(mode=0, q=0.0, dq=0.0, ddq=0.0, tau_est=0.0, q_raw=0.0, dq_raw=0.0, ddq_raw=0.0, temperature=0, lost=0, reserve=[0, 0]), 
+MotorState_(mode=0, q=0.0, dq=0.0, ddq=0.0, tau_est=0.0, q_raw=0.0, dq_raw=0.0, ddq_raw=0.0, temperature=0, lost=0, reserve=[0, 0]), 
+MotorState_(mode=0, q=0.0, dq=0.0, ddq=0.0, tau_est=0.0, q_raw=0.0, dq_raw=0.0, ddq_raw=0.0, temperature=0, lost=0, reserve=[0, 0]), 
+MotorState_(mode=0, q=0.0, dq=0.0, ddq=0.0, tau_est=0.0, q_raw=0.0, dq_raw=0.0, ddq_raw=0.0, temperature=0, lost=0, reserve=[0, 0]), 
+MotorState_(mode=0, q=0.0, dq=0.0, ddq=0.0, tau_est=0.0, q_raw=0.0, dq_raw=0.0, ddq_raw=0.0, temperature=0, lost=0, reserve=[0, 0]), 
+MotorState_(mode=0, q=0.0, dq=0.0, ddq=0.0, tau_est=0.0, q_raw=0.0, dq_raw=0.0, ddq_raw=0.0, temperature=0, lost=0, reserve=[0, 0])]
---
+++ example/low_level/motor_state_data_example_standup.py
@@ -0,0 +1,40 @@
+# Front right hip  ID: 0
+[MotorState_(mode=1, q=0.05751991271972656, dq=-0.054257333278656006, ddq=0.0, tau_est=2.7459492683410645, q_raw=0.0, dq_raw=0.0, ddq_raw=0.0, temperature=46, lost=0, reserve=[0, 632]), 
+ # Front right thigh  ID: 1
+ MotorState_(mode=1, q=0.8208380937576294, dq=0.003875523805618286, ddq=0.0, tau_est=-0.9895312786102295, q_raw=0.0, dq_raw=0.0, ddq_raw=0.0, temperature=30, lost=0, reserve=[0, 632]), 
+ # Front right calf  ID: 2
+ MotorState_(mode=1, q=-1.4774361848831177, dq=-0.010110062547028065, ddq=0.0, tau_est=5.5001444816589355, q_raw=0.0, dq_raw=0.0, ddq_raw=0.0, temperature=34, lost=0, reserve=[0, 632]), 
+ 
+
+ # Front left hip  ID: 3
+ MotorState_(mode=1, q=-0.06135064363479614, dq=-0.015502095222473145, ddq=0.0, tau_est=-2.1274921894073486, q_raw=0.0, dq_raw=0.0, ddq_raw=0.0, temperature=45, lost=0, reserve=[0, 632]), 
+  # Front left thigh  ID: 4
+ MotorState_(mode=1, q=0.800249457359314, dq=0.05038180947303772, ddq=0.0, tau_est=-0.37107422947883606, q_raw=0.0, dq_raw=0.0, ddq_raw=0.0, temperature=31, lost=0, reserve=[0, 632]), 
+  # Front left calf  ID: 5
+ MotorState_(mode=1, q=-1.478352427482605, dq=-0.02022012509405613, ddq=0.0, tau_est=6.02170991897583, q_raw=0.0, dq_raw=0.0, ddq_raw=0.0, temperature=36, lost=0, reserve=[0, 632]), 
+
+
+# Rear right hip  ID: 6
+ MotorState_(mode=1, q=0.07304714620113373, dq=0.0, ddq=0.0, tau_est=1.3606054782867432, q_raw=0.0, dq_raw=0.0, ddq_raw=0.0, temperature=49, lost=0, reserve=[0, 632]), 
+ # Rear right hip  ID: 7
+ MotorState_(mode=1, q=0.7756586074829102, dq=-0.05038180947303772, ddq=0.0, tau_est=-0.22264453768730164, q_raw=0.0, dq_raw=0.0, ddq_raw=0.0, temperature=31, lost=0, reserve=[0, 632]), 
+ # Rear right hip  ID: 8
+ MotorState_(mode=1, q=-1.4747347831726074, dq=0.012132074683904648, ddq=0.0, tau_est=4.3147687911987305, q_raw=0.0, dq_raw=0.0, ddq_raw=0.0, temperature=32, lost=0, reserve=[0, 632]), 
+ 
+
+ # Rear left hip  ID: 9
+ MotorState_(mode=1, q=-0.059301137924194336, dq=-0.007751047611236572, ddq=0.0, tau_est=-1.9048477411270142, q_raw=0.0, dq_raw=0.0, ddq_raw=0.0, temperature=51, lost=0, reserve=[0, 632]), 
+ # Rear left hip  ID: 10
+ MotorState_(mode=1, q=0.7784138917922974, dq=0.03875523805618286, ddq=0.0, tau_est=-0.07421484589576721, q_raw=0.0, dq_raw=0.0, ddq_raw=0.0, temperature=31, lost=0, reserve=[0, 632]), 
+ # Rear left hip  ID: 11
+ MotorState_(mode=1, q=-1.4332362413406372, dq=-0.02022012509405613, ddq=0.0, tau_est=4.2199387550354, q_raw=0.0, dq_raw=0.0, ddq_raw=0.0, temperature=33, lost=0, reserve=[0, 632]), 
+
+
+ MotorState_(mode=0, q=0.0, dq=0.0, ddq=0.0, tau_est=0.0, q_raw=0.0, dq_raw=0.0, ddq_raw=0.0, temperature=0, lost=0, reserve=[0, 0]), 
+ MotorState_(mode=0, q=0.0, dq=0.0, ddq=0.0, tau_est=0.0, q_raw=0.0, dq_raw=0.0, ddq_raw=0.0, temperature=0, lost=0, reserve=[0, 0]), 
+ MotorState_(mode=0, q=0.0, dq=0.0, ddq=0.0, tau_est=0.0, q_raw=0.0, dq_raw=0.0, ddq_raw=0.0, temperature=0, lost=0, reserve=[0, 0]), 
+ MotorState_(mode=0, q=0.0, dq=0.0, ddq=0.0, tau_est=0.0, q_raw=0.0, dq_raw=0.0, ddq_raw=0.0, temperature=0, lost=0, reserve=[0, 0]), 
+ MotorState_(mode=0, q=0.0, dq=0.0, ddq=0.0, tau_est=0.0, q_raw=0.0, dq_raw=0.0, ddq_raw=0.0, temperature=0, lost=0, reserve=[0, 0]), 
+ MotorState_(mode=0, q=0.0, dq=0.0, ddq=0.0, tau_est=0.0, q_raw=0.0, dq_raw=0.0, ddq_raw=0.0, temperature=0, lost=0, reserve=[0, 0]), 
+ MotorState_(mode=0, q=0.0, dq=0.0, ddq=0.0, tau_est=0.0, q_raw=0.0, dq_raw=0.0, ddq_raw=0.0, temperature=0, lost=0, reserve=[0, 0]), 
+MotorState_(mode=0, q=0.0, dq=0.0, ddq=0.0, tau_est=0.0, q_raw=0.0, dq_raw=0.0, ddq_raw=0.0, temperature=0, lost=0, reserve=[0, 0])]
